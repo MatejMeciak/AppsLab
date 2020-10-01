ccf53244007e7dc6035c154a24907443eec81776
Matej <matej.meciak123@gmail.com>
2020-10-01 20:45:14 +0200

    Progressing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Student student1 = new Student("John", 12345, 543452,"01001 Zilina");
+        Student student2 = new Student("John", 12345, 413453,"01001 Zilina" );
+
+        Person person1 = new Person("John", 21, 29000);
+        Person person2 = new Person("Steve", 32, 32000);
+        Person person3 = new Person("Martin", 16, 1600);
+
+        JobPosition jobPosition = new JobPosition("IT programmer", 2100);
+        Date date = new Date(1990, Calendar.AUGUST, 4);
+        Employee employee = new Employee("Jozko", "Mrkvicka", date, jobPosition);
+
         System.out.println("1. ) " + totalDistance(0.2, 0.4, 100.0));
         System.out.println("2. ) " + equal(3, 4, 3));
         System.out.println("3. ) " + isTriangle(2, 3, 4));
@@ -14,6 +25,13 @@ public class Main {
         System.out.println("9. ) " + warOfNumbers(new int[] {2, 3, 1, 0}));
         System.out.println("10. ) " + checkEnding("abc", "bc"));
         System.out.println("11. ) " + canCapture("F5", "C8"));
+        System.out.println("12.a ) " + student1.getName() + ": " + student1.getStudentNumber());
+        System.out.println("12.b ) " + student1.getStudentNumber() + ", " + student1.getPhoneNumber() + ", " + student1.getAddress());
+        System.out.println("      " + student2.getStudentNumber() + ", " + student2.getPhoneNumber() + ", " + student2.getAddress());
+        System.out.println("13. ) " + calculateBudget(new Person[]{ person1, person2, person3}));
+        System.out.println("14. ) Area: " + Triangle.getArea(new Triangle(3, 4,5)));
+        System.out.println("      Perimeter: " + Triangle.getPerimeter(new Triangle(3, 4,5)));
+        System.out.println("15. ) " + employee.name + " " +employee.surname + ", " + employee.jobPosition.name + ", salary: " + employee.jobPosition.salary + " eur.");
     }
 
     public static double totalDistance(double stairHeight, double stairLength, double towerLength) {
@@ -71,7 +89,7 @@ public class Main {
         return Math.abs(array[array.length - 1] - array[0]);
     }
     public static boolean checkEnding(String first, String second){
-        StringBuilder sb = new StringBuilder();
+        var sb = new StringBuilder();
         sb.append(first, first.length() - 1, second.length());
         return sb.toString().equals(second);
     }
@@ -84,5 +102,140 @@ public class Main {
     public static int warOfNumbers(int[] numbers) {
         return 0;
     }
+    public static int calculateBudget(Person[] people){
+        int totalBudget = 0;
+        for (Person p:people){
+            totalBudget += p.budget;
+        }
+        return totalBudget;
+    }
 
+}
+class Student {
+    String name;
+    String address;
+    int studentNumber;
+    int phoneNumber;
+
+    public Student(String name, int studentNumber, int phoneNumber, String address) {
+        this.name = name;
+        this.studentNumber = studentNumber;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+}
+class Person {
+    String name;
+    int age;
+    int budget;
+
+    public Person(String name, int age, int budget) {
+        this.name = name;
+        this.age = age;
+        this.budget = budget;
+    }
+}
+class Triangle {
+    int a;
+    int b;
+    int c;
+
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public static int getPerimeter(Triangle triangle){
+        return triangle.a + triangle.b + triangle.c;
+    }
+
+    public static int getArea(Triangle triangle){
+        int s = (triangle.a + triangle.b + triangle.c) / 2;
+        return (int) Math.sqrt(s*(s - triangle.a)*(s - triangle.b)*(s- triangle.c));
+    }
+}
+class Employee{
+    String name;
+    String surname;
+    Date dateOfBirth;
+    JobPosition jobPosition;
+
+    public Employee(String name, String surname, Date dateOfBirth, JobPosition jobPosition) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.jobPosition = jobPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public JobPosition getJobPosition() {
+        return jobPosition;
+    }
+
+    public void setJobPosition(JobPosition jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+}
+class JobPosition{
+    String name;
+    double salary;
+
+    public JobPosition(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
